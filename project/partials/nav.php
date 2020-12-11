ef886b703e19c523226d87d08f44524feb8fa524
Christian Medina <christianmedina121@hotmail.com>
2020-12-11 20:59:50 +0000

    Adding working test_view_transactions.php file

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -15,6 +15,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
 	<li><a href="test_list_accounts.php">View Accounts</a></li>
 	<li><a href="test_create_transactions.php">Create Transaction</a></li>
 	<li><a href="test_edit_transactions.php">Edit Transaction</a></li>
+	<li><a href="test_view_transactions.php">View Transactions</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
--- project/test_edit_transactions.php
+++ project/test_edit_transactions.php
@@ -11,31 +11,52 @@ if (!has_role("Admin")) {
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
     $id2 = $id + 1;
+
+    $db = getDB();
+    $stmt = $db->prepare("SELECT amount, expected_total FROM Transactions where id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $balance = $result["expected_total"] - $result["amount"]; //calculating what the previous balance used to be
+
+    $stmt = $db->prepare("SELECT amount, expected_total FROM Transactions where id = :id2");
+    $r = $stmt->execute([":id2" => $id2]);
+    $result2 = $stmt->fetch(PDO::FETCH_ASSOC);
+    $balance2 = $result2["expected_total"] - $result2["amount"];
 }
+/*
+else {
+    flash("ID isn't set, we need an ID in order to update");
+}
+*/
 ?>
 <?php
 //saving
 if (isset($_POST["save"])) {
     //TODO add proper validation/checks
     $amount = $_POST["amount"];
+    $amount2 = $amount *-1;
+    $a1total = $balance + $amount;
+    $a2total = $balance2 + $amount2;
     $type = $_POST["type"];
     $memo = $_POST["memo"];
     $user = get_user_id();
     $db = getDB();
     if (isset($id)) {
-        $stmt = $db->prepare("UPDATE Transactions set amount=:amount, type=:type, memo=:memo, where id=:id");
+        $stmt = $db->prepare("UPDATE Transactions set amount=:amount, action_type=:type, memo=:memo, expected_total=:a1total where id=:id");
         $r = $stmt->execute([
             ":amount" => $amount,
             ":type" => $type,
             ":memo" => $memo,
+	    ":a1total" => $a1total,
             ":id" => $id
         ]);
-	$stmt = $db->prepare("UPDATE Transactions set amount=:amount, type=:type, memo=:memo, where id=:id");
+	$stmt = $db->prepare("UPDATE Transactions set amount=:amount2, action_type=:type, memo=:memo, expected_total=:a2total where id=:id2");
         $r2 = $stmt->execute([
-            ":amount" => $amount,
+            ":amount2" => $amount2,
             ":type" => $type,
             ":memo" => $memo,
-            ":id" => $id2
+	    ":a2total" => $a2total,
+            ":id2" => $id2
         ]);
         if ($r && $r2) {
             flash("Updated successfully with id: " . $id);
@@ -50,12 +71,12 @@ if (isset($_POST["save"])) {
     }
 }
 ?>
+
 <?php
 //fetching the transaction
 $result = [];
 $result2 = []; //second transaction
 if (isset($id)) {
-    $id = $_GET["id"];
     $db = getDB();
     $stmt = $db->prepare("SELECT * FROM Transactions where id = :id");
     $r = $stmt->execute([":id" => $id]);
@@ -63,29 +84,22 @@ if (isset($id)) {
 
     $id2 = $id + 1;
     $stmt = $db->prepare("SELECT * FROM Transactions where id = :id");
-    $r = stmt->execute([":id" => $id2]);
-    $result2 = stmt->fetch(PDO::FETCH_ASSOC);
+    $r = $stmt->execute([":id" => $id2]);
+    $result2 = $stmt->fetch(PDO::FETCH_ASSOC);
 }
-/*
-//get eggs for dropdown
-$db = getDB();
-$stmt = $db->prepare("SELECT id,name from F20_Eggs LIMIT 10");
-$r = $stmt->execute();
-$eggs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-*/
 ?>
     <h3>Edit Transaction</h3>
     <form method="POST">
         <label>Amount</label>
         <input name="amount" placeholder="amount" value="<?php echo $result["amount"]; ?>"/>
-        <label>Type</label>
+        <label>Type (Currently <?php echo $result["action_type"];?>)</label>
         <select name="type" value="<?php echo $result["action_type"];?>" >
             <option value="Deposit">Deposit</option>
             <option value="Withdraw">Withdraw</option>
 	    <option value="Transfer">Transfer</option>
         </select>
         <label>Memo</label>
-        <input type="text" min="1" name="memo" value="<?php echo $result["memo"]; ?>"/>
+        <input type="text" name="memo" value="<?php echo $result["memo"]; ?>"/>
         <input type="submit" name="save" value="Update"/>
     </form>
 
